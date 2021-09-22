858064bca6994319dd196d33edc8d6d0023d3281
Arsen Baktiyarov <devbaktiyarov@gmail.com>
2021-09-22 21:32:14 +0600

    create mockmvc test for IndexControllerTest

--- src/test/java/com/arsenbaktiyarov/spring/employees/controller/IndexControllerTest.java
+++ src/test/java/com/arsenbaktiyarov/spring/employees/controller/IndexControllerTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.ArrayList;
@@ -16,6 +19,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -31,6 +37,15 @@ class IndexControllerTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         indexController = new IndexController(employeeService);
+    }
+
+    @Test
+    void textMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/index/"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("index"));
     }
 
     @Test
